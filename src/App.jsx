771106f0697b0e9c350507afe0d097747d48ff76
suraj-chhabra-3 <surajchhabra3333@gmail.com>
2025-11-14 20:43:45 +0530

    message

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+     <h1 className='text-red-500 text-4xl font-bold'>
+      Tailwind is Working!
+    </h1>
+  );
+}
+
+export default App
